e07f8be05215eeb979c79623159216f5c27c434c
Marks Sunins <marks.sunins@gmail.com>
2025-04-16 12:12:38 +0000

    add support for fcos/rhcos ISO installation

--- single-node/init-fcos.sh
+++ single-node/init-fcos.sh
@@ -11,18 +11,18 @@ else
 fi
 
 # Get OKD latest version
-OKD_VERSION=$(curl -s https://api.github.com/repos/okd-project/okd/releases/latest | jq -r '.tag_name')
+OKD_VERSION=$(curl -s https://api.github.com/repos/okd-project/okd-scos/releases/latest | jq -r '.tag_name')
 
 OC_TAR_NAME="oc.tar.gz"
 INSTALLER_TAR_NAME="openshift-install-linux.tar.gz"
 
 # Download the OKD client (oc) and make it available for use by entering the following commands
-curl -L https://github.com/okd-project/okd/releases/download/$OKD_VERSION/openshift-client-linux-$POST_ARCH-$OKD_VERSION.tar.gz -o $OC_TAR_NAME
-tar zxf $OC_TAR_NAME
+curl -L https://github.com/okd-project/okd-scos/releases/download/$OKD_VERSION/openshift-client-linux-$POST_ARCH-$OKD_VERSION.tar.gz -o $OC_TAR_NAME
+tar zxvf $OC_TAR_NAME
 chmod +x oc
 
 # Download the OKD installer and make it available for use by entering the following commands
-curl -L https://github.com/okd-project/okd/releases/download/$OKD_VERSION/openshift-install-linux-$POST_ARCH-$OKD_VERSION.tar.gz -o $INSTALLER_TAR_NAME
+curl -L https://github.com/okd-project/okd-scos/releases/download/$OKD_VERSION/openshift-install-linux-$POST_ARCH-$OKD_VERSION.tar.gz -o $INSTALLER_TAR_NAME
 tar zxvf $INSTALLER_TAR_NAME
 chmod +x openshift-install
 
@@ -34,11 +34,13 @@ fi
 rm -f $OC_TAR_NAME $INSTALLER_TAR_NAME
 
 # Retrieve the FCOS ISO
-ISO_URL=$(./openshift-install coreos print-stream-json | jq -r ".architectures.$ARCH.artifacts.metal.formats.iso.disk.location" )
+ISO_URL=$(./openshift-install-fips coreos print-stream-json | jq -r ".architectures.$ARCH.artifacts.metal.formats.iso.disk.location" )
 curl -L $ISO_URL -o fcos-live.iso
 
+cp ./install-config.yaml ./sno/install-config.yaml
+
 # Generate OKD assets
-./openshift-install --dir=sno create single-node-ignition-config
+./openshift-install-fips --dir=sno create single-node-ignition-config
 
 # Embed the ignition data into the FCOS ISO
 podman run --privileged --pull always --rm \
